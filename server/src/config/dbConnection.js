f47850ae9e7ec88642aa1142213a74314118852e
RutvikMakvana4 <rutvikmakvana432@gmail.com>
2025-06-15 14:08:58 +0530

    changes the databsase connection issue

--- server/src/config/dbConnection.js
+++ server/src/config/dbConnection.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export default mongoose
-    .connect(`${process.env.MONGO_URL}`)
+    .connect(`${process.env.DATABASE_URL}`)
     .then(() => console.log("Database connected successfully"))
     .catch((error) => console.log("Database not connected", error));
--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,18 @@
 import "dotenv/config";
 import express from "express";
-// import "./config/dbConnection.js";
+import "./config/dbConnection.js";
+import cors from "cors";
+import quizRoutes from "./routes/quiz.js";
+import leaderboardRoutes from "./routes/leaderboard.js";
+import startQuestionCron from "./cron/questionCron.js";
+import startWinnerEmailCron from "./cron/winnerEmailCron.js";
 
 const app = express();
 
 const HOST = process.env.HOST;
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -13,6 +20,12 @@ app.use(express.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
   res.status(200).send("App is working!");
 });
+
+app.use('/api/quiz', quizRoutes);
+app.use('/api/leaderboard', leaderboardRoutes);
+
+startQuestionCron();
+startWinnerEmailCron();
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://${HOST}:${PORT}`);
